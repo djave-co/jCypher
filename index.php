83b724819bc95838095108d64ed0fca826d9a03d
Dave <sheepysheep60@hotmail.com>
2012-11-27 14:22:12 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<title>Cypher</title>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script src="js/cypher.js"></script>
+	<style type="text/css">
+		*{font-family: 	"Trebuchet MS", sans-serif;}
+		html, body{background:#222; color:#979C9E;}
+		h1{color:#616263;}
+		a{color:#AB7BE9;}
+
+		.wrapper{width:960px; margin: 0 auto;}
+		td{padding:10px;}
+	</style>
+</head>
+<body>
+<div id="output"></div>
+</body>
+</html>
